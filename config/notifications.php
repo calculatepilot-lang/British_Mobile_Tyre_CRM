3101b3d2c13acc9ab23b75b3203aea200ac2b024
Calculate Pilot <calculatepilot@gmail.com>
2026-08-27 04:42:56 +0500

    Add daily WhatsApp reporting configuration

---
+++ config/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'daily_report' => [
+        'enabled' => true,
+        'channel' => 'whatsapp',
+        'schedule_timezone' => 'Europe/London',
+        'default_time' => '08:00',
+        'delivery_mode' => 'template',
+        'approval_required_for_sending' => false,
+    ],
+];
